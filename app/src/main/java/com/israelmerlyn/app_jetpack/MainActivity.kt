ccc5d58e90896b9e17984e13604155cc844b0c9f
IsraelMerlyn <merlyn142a@gmail.com>
2024-12-15 13:05:10 -0600

    tasks #0.4 Creando Home - Scaffold

--- app/src/main/java/com/israelmerlyn/app_jetpack/MainActivity.kt
+++ app/src/main/java/com/israelmerlyn/app_jetpack/MainActivity.kt
@@ -12,13 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.israelmerlyn.app_jetpack.ui.theme.App_JetpackTheme
+import com.israelmerlyn.app_jetpack.views.HomeView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+        HomeView()
         }
     }
 }
--- app/src/main/java/com/israelmerlyn/app_jetpack/views/HomeView.kt
+++ app/src/main/java/com/israelmerlyn/app_jetpack/views/HomeView.kt
@@ -1,12 +1,42 @@
 package com.israelmerlyn.app_jetpack.views
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.israelmerlyn.app_jetpack.components.TitleBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeView (){
 
+    Scaffold (
+        topBar = {
+          CenterAlignedTopAppBar(
+              title = { TitleBar(name = "Home") }, colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                  containerColor = Color.Red
+              )
+          )
+        }
+    ) {
+        ContentHomeView()
+    }
 }
 
+@Composable
 fun ContentHomeView(){
-
+        Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment =Alignment.CenterHorizontally) {
+            Text(text = "Home View")
+            Text(text = "Home View")
+        }
 }
+
