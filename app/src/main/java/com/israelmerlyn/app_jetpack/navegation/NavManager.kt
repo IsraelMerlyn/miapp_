266334af4a6962975a52206166a4455bb3539d40
IsraelMerlyn <merlyn142a@gmail.com>
2025-01-03 22:25:23 -0600

    tasks #21 Estructurando carpetas de la VIEW Descuento

--- app/src/main/java/com/israelmerlyn/app_jetpack/navegation/NavManager.kt
+++ app/src/main/java/com/israelmerlyn/app_jetpack/navegation/NavManager.kt
@@ -3,18 +3,15 @@ package com.israelmerlyn.app_jetpack.navegation
 
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.israelmerlyn.app_jetpack.viewModels.CalcularViewModels1
-import com.israelmerlyn.app_jetpack.views.DescuentoView
+import com.israelmerlyn.app_jetpack.viewModels.descuento.CalcularViewModels1
+import com.israelmerlyn.app_jetpack.views.descuento.DescuentoView
 import com.israelmerlyn.app_jetpack.views.DetailsView
 import com.israelmerlyn.app_jetpack.views.HomeView
-import androidx.activity.viewModels
-import androidx.lifecycle.findViewTreeViewModelStoreOwner
 import com.israelmerlyn.app_jetpack.viewModels.loteria.LoteriaViewModels
 import com.israelmerlyn.app_jetpack.views.loteria.LoteriaView
 
